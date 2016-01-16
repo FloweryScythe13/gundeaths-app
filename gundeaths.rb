b9dfb1cc9b9931ef809694974a66ddceffcc78c4
Eric <brichett13@gmail.com>
2016-01-16 16:55:59 -0600

    first commit, laying out basic approach after doing some research

---
+++ gundeaths.rb
@@ -0,0 +1,7 @@
+##A Simple Gun Death Statistics App
+
+##The basic idea for this app is to query data on gun deaths in the United States and compare them over a few different quantities, 
+##such as year or the type of weapon involved. I will attempt to do this in a simple way by pulling compiled data from Gun Violence Archive's 
+##online records via web scraping, followed by manually calculating and displaying the statistical differences in this script. 
+
+require 'nokogiri'
